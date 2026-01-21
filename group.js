1a9778f64bf6511b70541645cb9b5b78a96681d2
matttttt <5334392+matttttt@users.noreply.github.com>
2026-01-21 18:29:06 -0500

    Update group.js

--- group.js
+++ group.js
@@ -1,10 +1,10 @@
 import fs from "fs";
 import path from "path";
 import { LANGUAGES_URL } from "./constants.js";
-import { getManifestId, getManifestIdFromImageTracker } from "./services/main.js";
+import { getManifestId } from "./services/main.js";
 
 const args = process.argv.slice(2);
-const isForce = args.includes('--force');
+const isForce = args.includes("--force");
 
 const inputFilePathsTemplate = [
     "./public/api/{lang}/agents.json",
@@ -17,13 +17,13 @@ const inputFilePathsTemplate = [
     "./public/api/{lang}/patches.json",
     "./public/api/{lang}/skins_not_grouped.json",
     "./public/api/{lang}/stickers.json",
+    "./public/api/{lang}/sticker_slabs.json",
     "./public/api/{lang}/keychains.json",
     "./public/api/{lang}/tools.json",
 ];
 
 let existingManifestId = "";
 const latestManifestId = await getManifestId();
-const latestManifestIdInImageTracker = await getManifestIdFromImageTracker();
 
 try {
     existingManifestId = fs.readFileSync("./manifestIdGroup.txt");
@@ -34,17 +34,15 @@ try {
 }
 
 if (isForce) {
-    console.log("Force flag detected, generating new data regardless of manifest Ids")
+    console.log("Force flag detected, generating new data regardless of manifest Ids");
 } else {
     // TODO: Need to check if default_generated.json from counter-strike-image-tracker repo has changed,
     // since we now pull data from there too.
-    if (existingManifestId == latestManifestId && existingManifestId == latestManifestIdInImageTracker) {
+    if (existingManifestId == latestManifestId) {
         console.log("Latest manifest Id matches existing manifest Id, exiting");
         process.exit(0);
     } else {
-        console.log(
-            "Latest manifest Id does not match existing manifest Id, generating new data."
-        );
+        console.log("Latest manifest Id does not match existing manifest Id, generating new data.");
     }
 }
 
@@ -52,9 +50,7 @@ for (let langObj of LANGUAGES_URL) {
     const lang = langObj.folder;
     const allData = {};
 
-    const inputFilePaths = inputFilePathsTemplate.map((templatePath) =>
-        templatePath.replace("{lang}", lang)
-    );
+    const inputFilePaths = inputFilePathsTemplate.map(templatePath => templatePath.replace("{lang}", lang));
 
     for (let filePath of inputFilePaths) {
         const fullPath = path.join(process.cwd(), filePath);
@@ -63,7 +59,7 @@ for (let langObj of LANGUAGES_URL) {
         if (fs.existsSync(fullPath)) {
             const fileData = JSON.parse(fs.readFileSync(fullPath, "utf-8"));
             if (Array.isArray(fileData)) {
-                fileData.forEach((item) => {
+                fileData.forEach(item => {
                     allData[item.id] = item;
                 });
             }
